2ee4a762fc5da4e68be2b78da3fc793eacd03801
Johnny Barros <jbarros@dbcorp.com.br>
2021-10-27 20:52:41 -0300

    Add project files.

---
+++ Compras/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Compras.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
