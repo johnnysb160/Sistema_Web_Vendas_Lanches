4af3d36be8fd6f07cfd697d4062b64bf378b6f62
Johnny Barros <jbarros@dbcorp.com.br>
2021-10-29 21:11:09 -0300

    Criação de uma View, uma ViewModel e configurando a responsabilidade no padrão do modelo de domínio atendido por sua Model.

---
+++ Compras/Controllers/LancheController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Compras.Repositories.Interfaces;
+using Compras.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Compras.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        private readonly ICategoriaRepository _catergoriaRepository;
+
+        public LancheController(ILancheRepository lancheRepository, ICategoriaRepository catergoriaRepository)
+        {
+            _lancheRepository = lancheRepository;
+            _catergoriaRepository = catergoriaRepository;
+        }
+
+
+        public IActionResult List()
+        {
+            ViewBag.Lanche = "Lanche";
+            ViewData["Categoria"] = "Categoria";
+
+            var lancheListViewModel = new LancheListViewModel();
+            lancheListViewModel.Lanche = _lancheRepository.Lanche;
+            lancheListViewModel.CategoriaAtual = "Categoria Atual";
+            return View(lancheListViewModel);
+        }
+    }
+}
---
+++ Compras/ViewModels/LancheListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Compras.Models;
+
+namespace Compras.ViewModels
+{
+    public class LancheListViewModel
+    {
+        public IEnumerable<Lanche> Lanche { get; set; }
+        public string CategoriaAtual { get; set; }
+    }
+}
