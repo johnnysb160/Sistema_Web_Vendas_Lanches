b01a2d9f485bd93815f63d64c3420b5d9c978c6a
Johnny Barros <jbarros@dbcorp.com.br>
2021-11-10 22:10:38 -0300

    Criação da página de contato e iniciado o processo do carrinho de compras.

---
+++ Compras/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Compras.Models;
+using Compras.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Compras.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = new List<CarrinhoCompraItem>() { new CarrinhoCompraItem(), new CarrinhoCompraItem() };
+
+            _carrinhoCompra.CarrinhoCompraItem = itens;
+
+
+           //_carrinhoCompra.CarrinhoCompraItem = _carrinhoCompra.GetCarrinhoItem();
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+            return View(carrinhoCompraVM);
+        }
+    }
+}
